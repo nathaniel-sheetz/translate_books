93ed8bf385e7605b57f9de2171c3134182984a37
nathaniel-sheetz <242042646+nathaniel-sheetz@users.noreply.github.com>
2026-05-11 22:36:43 -0400

    chore: remove local temporary files

--- find_sermons.ps1
+++
@@ -1,4 +0,0 @@
-$content = Get-Content 'C:\Users\Nathaniel\Cursor\translate_books\debug_sermons_text.txt' -Raw
-$results = [regex]::Matches($content, '(?i)sermon[^\n\r]{0,40}')
-Write-Host "Total matches:" $results.Count
-$results | ForEach-Object { $_.Value } | Select-Object -First 50
